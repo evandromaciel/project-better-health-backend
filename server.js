14e2a8e605cb1446d70ede3fe62947daf0d402ea
evandromaciel <evandromaciel@hotmail.com>
2025-12-07 06:35:23 -0700

    Update server port to 5001

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = require('./app');
 // ======================
 
 // Get port from environment variables or use default port 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // ======================
 // Start Server
